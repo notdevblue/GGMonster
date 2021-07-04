e2003d5b657090199faf2bc917fefac8c437d38c
HomeHan <wooyeophan@gmail.com>
2021-07-04 21:57:36 +0900

    창 크기 에니메이션 테스트

--- _GGMonster/GGMosters CA/Assets/Scripts/Window/TestMove.cs
+++ _GGMonster/GGMosters CA/Assets/Scripts/Window/TestMove.cs
@@ -5,40 +5,15 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TestMove : MonoBehaviour
+public class TestMove : WindowCore
 {
-    public Text debugText;
+    float x = 1920.0f;
+    float y = 1080.0f;
 
-#if UNITY_STANDALONE_WIN || UNITY_EDITOR
-    [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
-    private static extern IntPtr GetActiveWindow();
-
-    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-    private static extern bool SetWindowPos(IntPtr hwnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
-    [DllImport("user32.dll", EntryPoint = "FindWindow")]
-    public static extern IntPtr FindWindow(string className, string windowName);
-
-    public static void SetPosition(int x, int y, int resX = 0, int resY = 0)
-    {
-        SetWindowPos(FindWindow(null, "GGMonsters"), 0, x, y, resX, resY, resX * resY == 0 ? 1 : 0);
-    }
-#endif
-
-    IntPtr ActiveHwnd;
-    private void Awake()
-    {
-        ActiveHwnd = GetActiveWindow();
-
-        //debugText.text =  SetWindowPos(ActiveHwnd, 0, 100, 100, 1280, 720, 1).ToString();
-
-    }
-
-
-    float degree = 0.0f;
-    public GameObject debugObject;
     private void FixedUpdate()
     {
-        SetWindowPos(ActiveHwnd, 0, 300, (int)((Mathf.Sin(degree += 0.1f)) * 100) + 150, 1280, 720, 1);
+        x = 1920.0f - (Mathf.Sin(Time.time * 2.0f) + 1.0f) * 500.0f;
+        y = 1080.0f - (Mathf.Sin(Time.time * 2.0f) + 1.0f) * 250.0f;
+        SetWindowSize((int)x, (int)y);
     }
-
 }
